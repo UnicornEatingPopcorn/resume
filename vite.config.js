326a7cc7e2ed403a9fa34fdbc4961f754c913972
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2024-09-14 22:18:27 +0900

    update baseUrl, remove script from html

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,16 @@ import pugPlugin from "vite-plugin-pug"
 const options = { }
 const locals = { name: "My Pug" }
 
+let baseUrl
+
+if (process.env.PROD) {
+  baseUrl = 'https://unicorneatingpopcorn.github.io/resume/'
+} else {
+  baseUrl = '/'
+}
 export default defineConfig({
   plugins: [pugPlugin(options, locals)],
   build: {outDir: "docs"},
-  base: 'https://unicorneatingpopcorn.github.io/resume/',
+  base: baseUrl,
   host: '0.0.0.0'
 })
