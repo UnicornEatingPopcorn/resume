92a622e0479d57d81f951d4bc43f34d63a4ee5e6
UnicornEatingPopcorn <unicorn.eating.popcorn@gmail.com>
2024-09-14 23:20:25 +0900

    resolve conflicts

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,9 @@ import pugPlugin from "vite-plugin-pug"
 const options = { }
 const locals = { name: "My Pug" }
 
-let baseUrl
-
-if (process.env.PROD) {
-  baseUrl = 'https://unicorneatingpopcorn.github.io/resume/'
-} else {
-  baseUrl = '/'
-}
 export default defineConfig({
   plugins: [pugPlugin(options, locals)],
   build: {outDir: "docs"},
-<<<<<<< Updated upstream
-  base: baseUrl,
-=======
   base: './',
->>>>>>> Stashed changes
   host: '0.0.0.0'
 })
